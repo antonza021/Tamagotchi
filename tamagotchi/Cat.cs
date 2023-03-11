dae7bfdb28dcb1ba1dc3f12159fc14f02725e37f
kudryavtsevaa <kudryavtsevaa@411D-8>
2023-03-11 11:52:39 +0700

    Добавьте файлы проекта.

---
+++ tamagotchi/Cat.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tamagotchi
+{
+        public struct Fruit
+    {
+        public int heath;
+        public string type;
+    }
+    class Cat
+    {
+        private string name;
+        private string color;
+        private int health;
+
+        public Cat(string name = "Дружочек", string color = "Пурпурный")
+        {
+            this.name = name;
+            this.color = color;
+            health = 100;
+        }
+        public static void Meow(Cat a)
+        {
+            Console.WriteLine("Meow");
+        }
+        public void Eat(Fruit f)
+        {
+            Console.WriteLine($"Кошка сьела {f.type}.{f.heath} здоровья");
+        }
+        public static void DoMeal(Cat c, Fruit f)
+        {
+            c.Eat(f);
+            c.health = c.health + f.heath;
+        }
+        public static void LapkiCarapki(Cat a)
+        {
+            Random rnd = new Random();
+            if (rnd.Next(1, 101) > 70)
+            {
+                int damage = 10;
+                a.health -= damage;
+            }
+            else
+            {
+                int damage = rnd.Next(1, 5);
+                a.health -= damage;
+            }
+        }
+        public static void Kus(Cat a)
+        {
+            Random rnd = new Random();
+            if (rnd.Next(1, 101) > 90)
+            {
+                int damage = 20;
+                a.health -= damage;
+            }
+            else
+            {
+                int damage = rnd.Next(1, 5);
+                a.health -= damage;
+            }
+        }
+        public static void Fight(Cat a, Cat b)
+        {
+            Console.WriteLine("Список ударов: 1. Удар лапкой, 2. злой кусь, 3. закруглени" +
+                "е битвы");
+            int u = int.Parse(Console.ReadLine());
+            switch (u)
+            {
+                case 1:
+                    Cat.LapkiCarapki(b);
+                    break;
+                case 2:
+                    Cat.Kus(b);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        public static void Info(Cat a)
+        {
+            Console.WriteLine($"{a.name}, {a.color}, {a.health}");
+        }
+
+
+    }
+}
